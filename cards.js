6b19e46a265856baf933489c7bae4daab40a9550
Eli Jackson <EliJackson@elis-mbp.attlocal.net>
2018-01-01 21:45:24 -0600

    setting variables on JS

---
+++ cards.js
@@ -0,0 +1,13 @@
+// Create an HTML page that contains a text area and a button labeled Create.
+// When the user enters in text into the text area and then clicks the create button, create a new card element in the DOM that includes it's own delete button. You decide the height/width of the card.
+
+let input = document.getElementById("userInput");
+let createButton = document.getElementById("create");
+let output = document.getElementById("outputField")
+
+
+
+
+
+
+// When the user clicks the Delete button, the containing card, and no other cards, should then be removed from the DOM. Not just made invisible, actually removed from the DOM.
